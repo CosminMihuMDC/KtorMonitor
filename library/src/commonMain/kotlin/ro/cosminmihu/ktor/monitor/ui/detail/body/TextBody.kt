e79dc9c263a17930e116f103f0e173091d0b7fe1
Cosmin Mihu <cosmin.mihu@gmail.com>
2025-12-29 18:12:27 +0200

    Refactor TextBody and TransactionSection to improve layout and selection handling

--- library/src/commonMain/kotlin/ro/cosminmihu/ktor/monitor/ui/detail/body/TextBody.kt
+++ library/src/commonMain/kotlin/ro/cosminmihu/ktor/monitor/ui/detail/body/TextBody.kt
@@ -1,7 +1,6 @@
 package ro.cosminmihu.ktor.monitor.ui.detail.body
 
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,7 +13,9 @@ internal fun TextBody(
     text: AnnotatedString,
     modifier: Modifier = Modifier
 ) {
-    SelectionContainer(modifier = modifier.verticalScroll(rememberScrollState())) {
-        Text(text = text, style = MaterialTheme.typography.bodyMedium)
-    }
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodyMedium,
+        modifier = modifier.verticalScroll(rememberScrollState())
+    )
 }
