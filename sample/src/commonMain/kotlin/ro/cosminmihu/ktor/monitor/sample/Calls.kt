fd96ebdaf1e89b23d3e0cae4efb7d4661c3a90f4
Cosmin Mihu <cosmin.mihu@gmail.com>
2025-12-30 11:32:20 +0200

    Enhance HTTP call handling by adding various request types and endpoints from https://httpbin.org/

--- sample/src/commonMain/kotlin/ro/cosminmihu/ktor/monitor/sample/Calls.kt
+++ sample/src/commonMain/kotlin/ro/cosminmihu/ktor/monitor/sample/Calls.kt
@@ -9,61 +9,100 @@ import io.ktor.client.request.put
 private const val httpbin = "https://httpbin.org"
 
 internal suspend fun makeCalls() {
-    // HTTP Methods
     with(httpClient()) {
-        runCatching { this.get(httpbin) }
-        runCatching { this.delete(httpbin) }
-        runCatching { this.patch(httpbin) }
-        runCatching { this.post(httpbin) }
-        runCatching { this.put(httpbin) }
-    }
+        // HTTP Methods
+        runCatching { this.delete("$httpbin/delete") }
+        runCatching { this.get("$httpbin/get") }
+        runCatching { this.patch("$httpbin/patch") }
+        runCatching { this.post("$httpbin/post") }
+        runCatching { this.put("$httpbin/put") }
 
-    // Images
-    with(httpClient()) {
-        runCatching { this.get("$httpbin/image") }
-        runCatching { this.get("$httpbin/image/png") }
-        runCatching { this.get("$httpbin/image/jpeg") }
-        runCatching { this.get("$httpbin/image/svg") }
-        runCatching { this.get("$httpbin/image/webp") }
-        runCatching { this.get("https://dkemhji6i1k0x.cloudfront.net/000_clients/896535/page/896535al1Ykfdt.gif") }
-    }
+        // Auth
+        runCatching { this.get("$httpbin/basic-auth/user/passwd") }
+        runCatching { this.get("$httpbin/bearer") }
+        runCatching { this.get("$httpbin/digest-auth/auth/user/passwd") }
+        runCatching { this.get("$httpbin/digest-auth/auth/user/passwd/MD5") }
+        runCatching { this.get("$httpbin/digest-auth/auth/user/passwd/MD5/stale_after") }
+        runCatching { this.get("$httpbin/hidden-basic-auth/user/passwd") }
 
-    // Response formats
-    with(httpClient()) {
+        // Status codes
+        runCatching { this.delete("$httpbin/status/200") }
+        runCatching { this.get("$httpbin/status/200") }
+        runCatching { this.patch("$httpbin/status/200") }
+        runCatching { this.post("$httpbin/status/200") }
+        runCatching { this.put("$httpbin/status/200") }
+
+        // Request inspection
+        runCatching { this.get("$httpbin/headers") }
+        runCatching { this.get("$httpbin/ip") }
+        runCatching { this.get("$httpbin/user-agent") }
+
+        // Response inspection
+        runCatching { this.get("$httpbin/cache") }
+        runCatching { this.get("$httpbin/cache/60") }
+        runCatching { this.get("$httpbin/etag/abc") }
+        runCatching { this.get("$httpbin/response-headers") }
+        runCatching { this.post("$httpbin/call/response-headers") }
+
+        // Response formats
+        runCatching { this.get("$httpbin/brotli") }
+        runCatching { this.get("$httpbin/deflate") }
+        runCatching { this.get("$httpbin/deny") }
+        runCatching { this.get("$httpbin/encoding/utf8") }
+        runCatching { this.get("$httpbin/gzip") }
         runCatching { this.get("$httpbin/html") }
-        runCatching { this.get("$httpbin/xml") }
         runCatching { this.get("$httpbin/json") }
         runCatching { this.get("$httpbin/robots.txt") }
-        runCatching { this.get("$httpbin/gzip") }
-        runCatching { this.get("$httpbin/encoding/utf8") }
-        runCatching { this.get("$httpbin/deny") }
-        runCatching { this.get("$httpbin/deflate") }
-        runCatching { this.get("$httpbin/brotli") }
-    }
+        runCatching { this.get("$httpbin/xml") }
 
-    // Request Error
-    with(httpClient()) {
-        runCatching { this.get("https://12345678") }
-        runCatching { this.get("abcdefghijklmnopqrstuvwxyz") } // TODO
-    }
+        // Dynamic data
+        runCatching { this.get("$httpbin/base64/SFRUUEJJTiBpcyBhd2Vzb21l") }
+        runCatching { this.get("$httpbin/bytes/1024") }
+        runCatching { this.delete("$httpbin/delay/3") }
+        runCatching { this.get("$httpbin/delay/3") }
+        runCatching { this.patch("$httpbin/delay/3") }
+        runCatching { this.post("$httpbin/delay/3") }
+        runCatching { this.put("$httpbin/delay/3") }
+        runCatching { this.get("$httpbin/drip") }
+        runCatching { this.get("$httpbin/links/10/0") }
+        runCatching { this.get("$httpbin/range/1024") }
+        runCatching { this.get("$httpbin/stream-bytes/1024") }
+        runCatching { this.get("$httpbin/stream/10") }
+        runCatching { this.get("$httpbin/uuid") }
 
-    // Cookies
-    with(httpClient()) {
+        // Cookies
         runCatching { this.get("$httpbin/cookies") }
         runCatching { this.get("$httpbin/cookies/delete") }
         runCatching { this.get("$httpbin/cookies/set") }
         runCatching { this.get("$httpbin/cookies/set/name/value") }
-    }
 
-    // Request inspection
-    with(httpClient()) {
-        runCatching { this.get("$httpbin/headers") }
-        runCatching { this.get("$httpbin/ip") }
-        runCatching { this.get("$httpbin/user-agent") }
-    }
+        // Images
+        runCatching { this.get("$httpbin/image") }
+        runCatching { this.get("$httpbin/image/jpeg") }
+        runCatching { this.get("$httpbin/image/png") }
+        runCatching { this.get("$httpbin/image/svg") }
+        runCatching { this.get("$httpbin/image/webp") }
 
-    // Javascript
-    with(httpClient()) {
-        runCatching { this.get("https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js") }
+        // Redirects
+        runCatching { this.get("$httpbin/absolute-redirect/3") }
+        runCatching { this.delete("$httpbin/redirect-to") }
+        runCatching { this.get("$httpbin/redirect-to") }
+        runCatching { this.patch("$httpbin/redirect-to") }
+        runCatching { this.post("$httpbin/redirect-to") }
+        runCatching { this.put("$httpbin/redirect-to") }
+        runCatching { this.get("$httpbin/redirect/3") }
+        runCatching { this.get("$httpbin/relative-redirect/3") }
+
+        // Anything
+        runCatching { this.delete("$httpbin/anything") }
+        runCatching { this.get("$httpbin/anything") }
+        runCatching { this.patch("$httpbin/anything") }
+        runCatching { this.post("$httpbin/anything") }
+        runCatching { this.put("$httpbin/anything") }
+        runCatching { this.delete("$httpbin/anything/test") }
+        runCatching { this.get("$httpbin/anything/test") }
+        runCatching { this.patch("$httpbin/anything/test") }
+        runCatching { this.post("$httpbin/anything/test") }
+        runCatching { this.put("$httpbin/anything/test") }
     }
 }
