65239c7a600467d891e65bd7dc412495d66dab64
Cosmin Mihu <cosmin.mihu@gmail.com>
2026-04-18 22:28:06 +0300

    Refactor KtorMonitorInterceptor constructor for clarity

--- okhttp/library-okhttp-no-op/src/commonMain/kotlin/ro/cosminmihu/ktor/monitor/KtorMonitorInterceptor.kt
+++ okhttp/library-okhttp-no-op/src/commonMain/kotlin/ro/cosminmihu/ktor/monitor/KtorMonitorInterceptor.kt
@@ -2,16 +2,13 @@ package ro.cosminmihu.ktor.monitor
 
 import okhttp3.Interceptor
 import okhttp3.Response
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.days
-import kotlin.time.Duration.Companion.hours
 
 /**
  * No-op implementation.
  */
-public class KtorMonitorInterceptor : Interceptor {
+public class KtorMonitorInterceptor() : Interceptor {
 
-    public constructor(block: KtorMonitorInterceptorConfig.() -> Unit)
+    public constructor(block: KtorMonitorInterceptorConfig.() -> Unit) : this()
 
     override fun intercept(chain: Interceptor.Chain): Response = chain.proceed(chain.request())
 }
