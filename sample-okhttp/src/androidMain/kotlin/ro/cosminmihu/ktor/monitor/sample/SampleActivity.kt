333b65a7001355a8c3c79cde66f65eccd8ddfeb5
Cosmin Mihu <cosmin.mihu@gmail.com>
2026-04-18 12:15:30 +0300

    Refactor sample activity and update application name: rename SampleOkHttpActivity to SampleActivity and modify app name strings

--- sample-okhttp/src/androidMain/kotlin/ro/cosminmihu/ktor/monitor/sample/SampleActivity.kt
+++ sample-okhttp/src/androidMain/kotlin/ro/cosminmihu/ktor/monitor/sample/SampleActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
-class SampleOkHttpActivity : ComponentActivity() {
+class SampleActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
