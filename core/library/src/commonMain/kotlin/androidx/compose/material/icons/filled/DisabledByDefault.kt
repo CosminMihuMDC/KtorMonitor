2a1825f75ef11160bd81d3fc07e1b2546b12c452
ketal <k@ketal.icu>
2026-07-07 11:13:23 +0800

    Remove material icons extended dependency

---
+++ core/library/src/commonMain/kotlin/androidx/compose/material/icons/filled/DisabledByDefault.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.material.icons.filled
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material.icons.materialPath
+import androidx.compose.ui.graphics.vector.ImageVector
+
+internal val Icons.Filled.DisabledByDefault: ImageVector
+    get() {
+        if (_disabledByDefault != null) {
+            return _disabledByDefault!!
+        }
+        _disabledByDefault = materialIcon(name = "Filled.DisabledByDefault") {
+            materialPath {
+                moveTo(3.0f, 3.0f)
+                verticalLineToRelative(18.0f)
+                horizontalLineToRelative(18.0f)
+                verticalLineTo(3.0f)
+                horizontalLineTo(3.0f)
+                close()
+                moveTo(17.0f, 15.59f)
+                lineTo(15.59f, 17.0f)
+                lineTo(12.0f, 13.41f)
+                lineTo(8.41f, 17.0f)
+                lineTo(7.0f, 15.59f)
+                lineTo(10.59f, 12.0f)
+                lineTo(7.0f, 8.41f)
+                lineTo(8.41f, 7.0f)
+                lineTo(12.0f, 10.59f)
+                lineTo(15.59f, 7.0f)
+                lineTo(17.0f, 8.41f)
+                lineTo(13.41f, 12.0f)
+                lineTo(17.0f, 15.59f)
+                close()
+            }
+        }
+        return _disabledByDefault!!
+    }
+
+private var _disabledByDefault: ImageVector? = null
